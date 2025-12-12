ab38573f2e00ebf7a98640154ee4150d680c9ce4
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-12 12:36:07 +0100

    LogicsImpl field and constructor

--- 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -1,14 +1,31 @@
 package it.unibo.es2;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LogicsImpl implements Logics {
+
+    private final Map<Pair<Integer, Integer>, Boolean> grid = new LinkedHashMap<>();
+
+    public LogicsImpl(final int size){
+
+        if (size < 4 | size > 7 ){
+            throw new IllegalArgumentException("Grid size must be in range 4-7 ");
+        }
+
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                grid.put(new Pair<>(i,j), false);
+            }    
+        }
+    }
     
     /**
      * {@inheritDoc}
      */
     @Override
     public void toggleSpot(Pair<Integer, Integer> position){
-        
+
     }
     
     /**
@@ -18,5 +35,5 @@ public class LogicsImpl implements Logics {
     public boolean isGameOver(Pair<Integer, Integer> position){
         return false;
     }
-    
+
 }
