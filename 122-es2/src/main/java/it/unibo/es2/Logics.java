f9987a35e3558021f9f9b0cb009ce003ee376211
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-12 12:20:54 +0100

    logics and logicsImpl signed

---
+++ 122-es2/src/main/java/it/unibo/es2/Logics.java
@@ -0,0 +1,19 @@
+package it.unibo.es2;
+
+
+public interface Logics {
+    
+    /**
+     * toggles the state of a spot in a given position x,y
+     * @param position
+     */
+    void toggleSpot(Pair<Integer, Integer> position);
+    
+    /**
+     * calculates if a new line is been created turning on the spot in the given osition
+     * @param position
+     * @return true if game is over
+     */
+    boolean isGameOver(Pair<Integer, Integer> position);
+    
+}
---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,22 @@
+package it.unibo.es2;
+
+
+public class LogicsImpl implements Logics {
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void toggleSpot(Pair<Integer, Integer> position){
+        
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isGameOver(Pair<Integer, Integer> position){
+        return false;
+    }
+    
+}
