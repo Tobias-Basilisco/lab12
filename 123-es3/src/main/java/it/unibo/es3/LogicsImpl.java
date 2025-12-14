f3992678a39ed9ddfac424d7d2894dcc714db0e7
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-14 11:28:30 +0100

    123 LogicsImpl signature

---
+++ 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.es3;
+
+import java.util.List;
+
+public class LogicsImpl implements Logics{
+    
+    /**
+     * {@inheritDoc}
+     */
+    public List<Integer> start(){
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Integer> nextStep(){
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean IsGameOver(){
+        return false;
+    }
+}
