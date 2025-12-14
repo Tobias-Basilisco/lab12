77136f506b99661bf7c31c80071f3efe9044d2ba
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-14 12:21:20 +0100

    pairToInt e intToPair

--- 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
+++ 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
@@ -1,5 +1,6 @@
 package it.unibo.es3;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -10,30 +11,45 @@ import java.util.Set;
 public class LogicsImpl implements Logics{
 
     private final Map<Pair<Integer, Integer>, Boolean> grid = new LinkedHashMap<>();
-    private int gridSize;
-    private int stageInedex;
+    private int gridWidth;
+    private int stageInedex = -1;
     private final Set<Pair<Integer, Integer>> startingPoints = new LinkedHashSet<>();
+    private int startingPointsQuantity;
 
-    public LogicsImpl(final int gridSize){
-        
-        if (gridSize < 2){
-            throw new IllegalArgumentException("grid size must be at least 2");
+    public LogicsImpl(final int gridWidth, final int startingPointsQuantity){
+
+        if (gridWidth < 2){
+            throw new IllegalArgumentException("grid width must be at least 2");
         }
 
-        this.gridSize = gridSize;
+        if (startingPointsQuantity < 1){
+            throw new IllegalArgumentException("at least 1 starting point is required");
+        }
 
-        for (int i = 0; i < gridSize; i++){
-            for (int j = 0; j < gridSize; j++){
+        this.gridWidth = gridWidth;
+
+        for (int i = 0; i < gridWidth; i++){
+            for (int j = 0; j < gridWidth; j++){
                 grid.put(new Pair<>(i,j), false);
             }    
         }
     }
-    
+
     /**
      * {@inheritDoc}
      */
     public List<Integer> start(){
-        return null;
+
+        final Random random = new Random();
+        int pointsCreated = 0;
+
+        while (pointsCreated < startingPointsQuantity){
+            int point = random.nextInt(gridWidth * gridWidth);
+
+        }
+
+        stageInedex++;
+        return Collections.unmodifiableList(null);
     }
 
     /**
@@ -48,5 +64,13 @@ public class LogicsImpl implements Logics{
      */
     public boolean IsGameOver(){
         return false;
+    }
+
+    private int pairToInt(final Pair<Integer, Integer> pair){
+        return pair.y() * gridWidth + pair.x();
+    }
+
+    private Pair<Integer, Integer> intToPair(final int i){
+        return new Pair<>( i % gridWidth, i / gridWidth);
     }
 }
