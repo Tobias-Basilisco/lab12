ae80f6c6b99e88b6e6c88b5345a1eb173f4b758e
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-14 11:18:07 +0100

    123 logics interface

---
+++ 123-es3/src/main/java/it/unibo/es3/Logics.java
@@ -0,0 +1,24 @@
+package it.unibo.es3;
+
+import java.util.List;
+
+public interface Logics {
+    
+    /**
+     * turns on the starting spots
+     * @return list of indexes to turn on
+     */
+    List<Integer> start();
+
+    /**
+     * turns on the required spots from game logic
+     * @return list of indexes to turn on
+     */
+    List<Integer> nextStep();
+
+    /**
+     * checks if game is over
+     * @return true if game is over
+     */
+    boolean IsGameOver();
+}
