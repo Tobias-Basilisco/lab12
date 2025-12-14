ab299c3e6edc0264b3b851431bb4b9a6aa0f38fc
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-14 11:56:57 +0100

    123 LogicsImpl fields and constructor

--- 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
+++ 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
@@ -1,8 +1,33 @@
 package it.unibo.es3;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 public class LogicsImpl implements Logics{
+
+    private final Map<Pair<Integer, Integer>, Boolean> grid = new LinkedHashMap<>();
+    private int gridSize;
+    private int stageInedex;
+    private final Set<Pair<Integer, Integer>> startingPoints = new LinkedHashSet<>();
+
+    public LogicsImpl(final int gridSize){
+        
+        if (gridSize < 2){
+            throw new IllegalArgumentException("grid size must be at least 2");
+        }
+
+        this.gridSize = gridSize;
+
+        for (int i = 0; i < gridSize; i++){
+            for (int j = 0; j < gridSize; j++){
+                grid.put(new Pair<>(i,j), false);
+            }    
+        }
+    }
     
     /**
      * {@inheritDoc}
