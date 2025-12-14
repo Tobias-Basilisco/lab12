c215567525eef6bed0e127921dcf9e25fc716d39
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-12-14 14:08:43 +0100

    nextStep button on GUI

--- 123-es3/src/main/java/it/unibo/es3/GUI.java
+++ 123-es3/src/main/java/it/unibo/es3/GUI.java
@@ -3,6 +3,8 @@ package it.unibo.es3;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.io.Serial;
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ public final class GUI extends JFrame {
     @Serial
     private static final long serialVersionUID = 1L;
     private final List<JButton> cells = new ArrayList<>();
+    private final String NEXT_TEXT = ">";
 
     /**
      * Constructor.
@@ -26,7 +29,7 @@ public final class GUI extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         // Create a panel with a grid layout
         final JPanel panel = new JPanel(new GridLayout(width, width));
-        this.getContentPane().add(panel);
+        this.getContentPane().add(BorderLayout.CENTER, panel);
         // Create buttons and add them to the panel
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < width; j++) {
@@ -37,6 +40,10 @@ public final class GUI extends JFrame {
                 panel.add(button);
             }
         }
+        //Create nextStep button and add it to the panel
+        final JButton nextStep = new JButton(NEXT_TEXT);
+        // nextStep.addActionListener(e -> );
+        this.getContentPane().add(BorderLayout.SOUTH, nextStep);
         pack();
         this.setVisible(true);
     }
